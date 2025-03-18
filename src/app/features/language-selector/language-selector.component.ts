0e62ca029accd8c1d2976f15d02003c6ee087f9f
Marko <devicmarko30@gmail.com>
2025-03-18 22:15:13 +0100

    fix a language flag bug after reseting showing wrong flag

--- src/app/features/language-selector/language-selector.component.ts
+++ src/app/features/language-selector/language-selector.component.ts
@@ -12,31 +12,31 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LanguageSelectorComponent {
   dropdownOpen = false;
-    currentFlag = localStorage.getItem('flag') || 'sr';
-    languages = [
-      { code: 'en', name: 'English', flag: '/flags/gb.png' },
-      { code: 'sr', name: 'Srpski', flag: '/flags/rs.png' },
-      { code: 'ru', name: 'Русский', flag: '/flags/ru.png' },
-      { code: 'de', name: 'Deutsch', flag: '/flags/de.png' }
-    ];
+  languages = [
+    { code: 'en', name: 'English', flag: '/flags/gb.png' },
+    { code: 'sr', name: 'Srpski', flag: '/flags/rs.png' },
+    { code: 'ru', name: 'Русский', flag: '/flags/ru.png' },
+    { code: 'de', name: 'Deutsch', flag: '/flags/de.png' }
+  ];
 
-    constructor(private translate: TranslateService) {
-      this.translate.setDefaultLang('sr');
-      this.translate.use(localStorage.getItem('lang') || 'sr');
-      this.currentFlag = '/flags/rs.png';
-      localStorage.setItem('flag', this.currentFlag);
-    }
-
-    toggleDropdown() {
-      this.dropdownOpen = !this.dropdownOpen;
-    }
+  currentFlag = localStorage.getItem('flag') || this.languages.find(lang => lang.code === 'sr')?.flag!;
   
-    changeLanguage(lang: string, flag: string) {
-      this.translate.use(lang);
-      localStorage.setItem('lang', lang);
-      localStorage.setItem('flag', flag);
-      this.currentFlag = flag;
-      this.dropdownOpen = false;
-    }
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('sr');
+    this.translate.use(localStorage.getItem('lang') || 'sr');
+    localStorage.setItem('flag', this.currentFlag);
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  changeLanguage(lang: string, flag: string) {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+    localStorage.setItem('flag', flag);
+    this.currentFlag = flag;
+    this.dropdownOpen = false;
+  }
 
 }
