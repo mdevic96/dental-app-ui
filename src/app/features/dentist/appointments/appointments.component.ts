7783b7f6f7f21e2f459b8a96dacb5fce16f785ca
mdevic <marko.devic@clarivate.com>
2025-03-29 23:42:28 +0100

    Add translation for dentist appointments page

--- src/app/features/dentist/appointments/appointments.component.ts
+++ src/app/features/dentist/appointments/appointments.component.ts
@@ -4,11 +4,14 @@ import { AuthService } from '../../auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppointmentDto } from '../../../core/appointment.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointments',
   imports: [
     CommonModule,
+    TranslateModule,
+    FormsModule,
     TranslateModule
   ],
   templateUrl: './appointments.component.html',
@@ -18,6 +21,8 @@ export class AppointmentsComponent implements OnInit {
   upcomingAppointments: AppointmentDto[] = [];
   pastAppointments: AppointmentDto[] = [];
   selectedTab: string = 'upcoming';
+  sortOrder: 'asc' | 'desc' = 'desc';
+
   private apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
@@ -36,7 +41,23 @@ export class AppointmentsComponent implements OnInit {
       .subscribe(appointments => this.upcomingAppointments = appointments.filter(a => a.status === 'SCHEDULED'));
 
     this.http.get<AppointmentDto[]>(`${this.apiUrl}/appointments/dentist/${dentistId}`, { headers })
-      .subscribe(appointments => this.pastAppointments = appointments.filter(a => a.status !== 'SCHEDULED'));
+      .subscribe(appointments => {
+        const filtered = appointments.filter(a => a.status !== 'SCHEDULED');
+        this.pastAppointments = this.sortAppointmentsByStatus(filtered);
+      });
+  }
+
+  sortPastAppointments(): void {
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.pastAppointments = this.sortAppointmentsByStatus(this.pastAppointments);
+  }
+  
+  sortAppointmentsByStatus(appointments: AppointmentDto[]): AppointmentDto[] {
+    return [...appointments].sort((a, b) => {
+      const statusA = a.status.toUpperCase();
+      const statusB = b.status.toUpperCase();
+      return this.sortOrder === 'asc' ? statusA.localeCompare(statusB) : statusB.localeCompare(statusA);
+    });
   }
 
   updateStatus(id: number, status: 'COMPLETED' | 'CANCELLED' | 'SCHEDULED'): void {
